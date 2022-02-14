9e0a254f91255cf2a044d3a8e8be211b72e371c3
siemen <siemensubbaiah1@gmail.com>
2022-02-14 21:10:17 +0530

    added progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,20 @@ import SSRProvider from 'react-bootstrap/SSRProvider';
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StateProvider from '../context/StateProvider';
+import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SSRProvider>
-      <StateProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </StateProvider>
-    </SSRProvider>
+    <>
+      <NextNprogress color='#f53855' height={2} />
+      <SSRProvider>
+        <StateProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </StateProvider>
+      </SSRProvider>
+    </>
   );
 }
 
