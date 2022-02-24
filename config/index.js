790d35b9bbab2e8d14a24c823b7f08a03f3b8e15
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-02-24 23:12:56 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const RAZORPAY_KEY = process.env.RAZORPAY_KEY;
 export const RAZORPAY_SECRET = process.env.RAZORPAY_SECRET;
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_FRONTEND_URL || '';
